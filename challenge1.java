7f20c0b778cf62217788d3b19bc927204cbf1fec
Vinoth <38490375+vinothhawk28@users.noreply.github.com>
2023-02-03 19:49:35 +0530

    Array

---
+++ challenge1.java
@@ -0,0 +1,11 @@
+
+public class challenge1 {
+public static void main(String[] args) {
+	int[] arr=new int[4];
+	arr[0]=10;
+	arr[1]=20;
+	arr[2]=30;
+	arr[3]=90;
+	System.out.println(arr[0]);
+}
+}
